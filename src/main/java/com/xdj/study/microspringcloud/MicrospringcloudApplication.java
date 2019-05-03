df6503d608dce1caccda76d25808b615bd3bd217
hqxdj <934282093@qq.com>
2019-05-03 23:01:04 +0800

    spring cloud初始化和gradle脚本学习

---
+++ src/main/java/com/xdj/study/microspringcloud/MicrospringcloudApplication.java
@@ -0,0 +1,13 @@
+package com.xdj.study.microspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrospringcloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrospringcloudApplication.class, args);
+    }
+
+}
